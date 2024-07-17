b0c211730046b872dd159839bd1c5bc202b1afc9
Fernando Rivera <1220514@usap.edu>
2024-07-17 15:44:23 -0600

    Import and Export Routes

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,22 @@ app.use(cors(
     config.application.cors.server
 ));
 
+// IMPORTAR RUTAS BACKEND
+const editorialRoute = require('./routes/editorialRoute');
+const paisRoute = require('./routes/paisRoute');
+const tipolibroRoute = require('./routes/tipolibroRoute');
+const libroRoute = require('./routes/libroRoute');
+const areaRoute = require('./routes/areaRoute');
+const autorRoute = require('./routes/autorRoute');
+const librosporareaRoute = require('./routes/librosporareaRoute');
+const librosporautorRoute = require('./routes/librosporautorRoute');
+const afiliadoRoute = require('./routes/afiliadoRoute');
+const tipoprestamoRoute = require('./routes/tipoprestamoRoute');
+const estadoRoute = require('./routes/estadoRoute');
+const librosporexistenciaRoute = require('./routes/librosporexistenciaRoute');
+const prestamoRoute = require('./routes/prestamoRoute');
+const estadoporexistenciaRoute = require('./routes/estadoporexistenciaRoute');
+
 app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
@@ -37,6 +53,24 @@ app.use(express.urlencoded({extended: false}));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
+// USAR RUTAS BACKEND
+app.use('/api/editorial', editorialRoute);
+app.use('/api/pais', paisRoute);
+app.use('/api/tipolibro', tipolibroRoute);
+app.use('/api/libro', libroRoute);
+app.use('/api/area', areaRoute);
+app.use('/api/autor', autorRoute);
+app.use('/api/librosporarea', librosporareaRoute);
+app.use('/api/librosporautor', librosporautorRoute);
+app.use('/api/afiliado', afiliadoRoute);
+app.use('/api/tipoprestamo', tipoprestamoRoute);
+app.use('/api/estado', estadoRoute);
+app.use('/api/librosporexistencia', librosporexistenciaRoute);
+app.use('/api/prestamo', prestamoRoute);
+app.use('/api/estadoporexistencia', estadoporexistenciaRoute);
+
+app.use(express.static(path.join(__dirname,'public')));
 
 // SERVER INIT
 app.listen(app.get('port'), () =>{
