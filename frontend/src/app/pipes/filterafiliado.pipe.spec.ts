87481e16180fdefca450862e20d1f3cb3bcf288c
2170523 <2170523@usap.edu>
2024-07-22 20:11:54 -0600

    Agregar pipe a Afiliado

---
+++ frontend/src/app/pipes/filterafiliado.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterafiliadoPipe } from './filterafiliado.pipe';
+
+describe('FilterafiliadoPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterafiliadoPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
